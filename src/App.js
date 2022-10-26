25b1081466253ecedda8aa2920ed351669360532
sellauno <sellauno03@gmail.com>
2022-10-26 20:20:10 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [context, setContext] = useState("");
+  const [done, setDone] = useState(false);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Study React",
+      context: "Let's study React!",
+      done: false
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div>My Todo List</div>
+        <div>React</div>
+      </div>
+      <input
+        type="text"
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        value={context}
+        onChange={(event) => {
+          setContext(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setTodos([...todos, { id: todos.length + 1, title: title, context: context, done: done }]);
+        }}
+      >
+        To Do
+      </button>
+
+      <h1>Todo List</h1>
+      <div className="todos-container">
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <div className="todo-title" >
+              {todo.title}
+            </div>
+            <div className="todo-context">
+              {todo.context}
+            </div>
+            <button className="deleteButton">Delete</button>
+            <button className="doneButton"
+              onClick={() => {
+                setDone(true)
+                setTodos([...todos, { id: todo.id, title: todo.title, context: todo.context, done: done }]);
+                // setTodos([...todos, { id: todos.id, done: done }]);
+              }}
+            >Done</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
